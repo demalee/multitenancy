7a61d7ee8e4f7f785c5c5c2a99038cb0445a82a6
allanboyd <allanochieng307@gmail.com>
2020-10-09 11:10:03 +0300

    theme service provider

--- app/Providers/ThemeServiceProvider.php
+++ app/Providers/ThemeServiceProvider.php
@@ -16,42 +16,43 @@ class ThemeServiceProvider extends ServiceProvider
      */
     public function register()
     {
-//        //
-//        $theme_name = "default";
-//        $theme_name_request = "agriculture";
-//        $theme_info = Theme::all();
-//
-//        foreach ($theme_info as $theme)
-//        {
-//            if ($theme_name_request == $theme->name)
-//            {
-//                $theme_name = $theme_name_request;
-//                break;
-//            }
-//        }
-//        View::share("THEME_NAME",$theme_name);
-//        $theme_directories = array("partials","layouts","includes","Errors","Home","About","Contact","Others");
-//
-//        foreach ($theme_directories as $theme_directory)
-//        {
-//            $theme_u_dir = "THEME_".strtoupper($theme_directory);
-//            $blade_path = "Themes.".$theme_name.".".$theme_directory.".";
-//            $default_blade_path = "Themes.default.".$theme_directory.".";
-//
-//            $path = resource_path("views/Themes/".$theme_name."/".$theme_directory);
-//            #echo "PATH: ".$path."<br>";
-//            if(file_exists($path) && is_dir($path))
-//            {
-//                View::share($theme_u_dir, $blade_path);
-//                Config::set($theme_u_dir , $blade_path);
-//            }
-//            else
-//            {
-//                View::share($theme_u_dir, $default_blade_path);
-//                Config::set($theme_u_dir , $default_blade_path);
-//            }
-//
-//        }
+        //
+        $theme_name = "default";
+        $theme_name_request = "agriculture";
+        $theme_info = Theme::all();
+        $theme_info = array("default","agriculture");
+
+        foreach ($theme_info as $theme)
+        {
+            if ($theme_name_request == $theme)
+            {
+                $theme_name = $theme_name_request;
+                break;
+            }
+        }
+        View::share("THEME_NAME",$theme_name);
+        $theme_directories = array("","partials","layouts","includes","errors","pages");
+
+        foreach ($theme_directories as $theme_directory)
+        {
+            $theme_u_dir = "THEME_".strtoupper($theme_directory);
+            $blade_path = "Themes.".$theme_name.".".$theme_directory.".";
+            $default_blade_path = "Themes.default.".$theme_directory.".";
+
+            $path = resource_path("views/Themes/".$theme_name."/".$theme_directory);
+            #echo "PATH: ".$path."<br>";
+            if(file_exists($path) && is_dir($path))
+            {
+                View::share($theme_u_dir, $blade_path);
+                Config::set($theme_u_dir , $blade_path);
+            }
+            else
+            {
+                View::share($theme_u_dir, $default_blade_path);
+                Config::set($theme_u_dir , $default_blade_path);
+            }
+
+        }
     }
 
     /**
