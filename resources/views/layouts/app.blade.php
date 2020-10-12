809697cdb5ab38354be2c01a5a7804ef529fb105
dev.demalee@gmail.com <dev.demalee@gmail.com>
2020-10-12 17:29:26 +0300

    assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,27 +12,46 @@
     <link rel="shortcut icon" href="http://laravel.pixelstrap.com/xolo/assets/images/favicon.png" type="image/x-icon">
     <title>Xolo - Premium Admin Template Login</title>
     <!-- Google font-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/fontawesome.css">
+    <!-- ico-font-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/icofont.css">
+    <!-- Themify icon-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/themify.css">
+    <!-- Flag icon-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/flag-icon.css">
+    <!-- Feather icon-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/feather-icon.css">
+    <!-- Plugins css start-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/prism.css">
+    <!-- Plugins css Ends-->
+    <!-- Bootstrap css-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+    <!-- App css-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <link id="color" rel="stylesheet" href="../assets/css/color-1.css" media="screen">
+    <!-- Responsive css-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/responsive.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700&amp;display=swap" rel="stylesheet">
     <!-- Font Awesome-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/fontawesome.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/fontawesome.css">
     <!-- ico-font-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/icofont.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/icofont.css">
     <!-- Themify icon-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/themify.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/themify.css">
     <!-- Flag icon-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/flag-icon.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/flag-icon.css">
     <!-- Feather icon-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/feather-icon.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/feather-icon.css">
     <!-- Plugins css start-->
     <!-- Plugins css Ends-->
     <!-- Bootstrap css-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/bootstrap.css">
     <!-- App css-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/style.css">
-    <link id="color" rel="stylesheet" href="http://laravel.pixelstrap.com/xolo/assets/css/color-1.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/style.css">
+    <link id="color" rel="stylesheet" href="https://laravel.pixelstrap.com/xolo/assets/css/color-1.css" media="screen">
     <!-- Responsive css-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/responsive.css">
 </head>
 <body>
 <!-- Loader starts-->
@@ -45,23 +64,43 @@
         @yield('content')
     </main>
 
-
-<!-- latest jquery-->
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/jquery-3.2.1.min.js"></script>
+<script src="../assets/js/jquery-3.2.1.min.js"></script>
 <!-- Bootstrap js-->
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/bootstrap/popper.min.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/bootstrap/bootstrap.js"></script>
+<script src="../assets/js/bootstrap/popper.min.js"></script>
+<script src="../assets/js/bootstrap/bootstrap.js"></script>
 <!-- feather icon js-->
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/icons/feather-icon/feather.min.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/icons/feather-icon/feather-icon.js"></script>
+<script src="../assets/js/icons/feather-icon/feather.min.js"></script>
+<script src="../assets/js/icons/feather-icon/feather-icon.js"></script>
+<!-- Sidebar jquery-->
+<script src="../assets/js/sidebar-menu.js"></script>
+<script src="../assets/js/config.js"></script>
+<!-- Plugins JS start-->
+<script src="../assets/js/chart/apex-chart/apex-chart.js"></script>
+<script src="../assets/js/chart/apex-chart/stock-prices.js"></script>
+<script src="../assets/js/prism/prism.min.js"></script>
+<script src="../assets/js/clipboard/clipboard.min.js"></script>
+<script src="../assets/js/counter/jquery.waypoints.min.js"></script>
+<script src="../assets/js/counter/jquery.counterup.min.js"></script>
+<script src="../assets/js/counter/counter-custom.js"></script>
+<script src="../assets/js/custom-card/custom-card.js"></script>
+<script src="../assets/js/dashboard/dashboard_4.js"></script>
+<script src="../assets/js/tooltip-init.js"></script>
+<!-- latest jquery-->
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap js-->
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/bootstrap/popper.min.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/bootstrap/bootstrap.js"></script>
+<!-- feather icon js-->
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/icons/feather-icon/feather.min.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/icons/feather-icon/feather-icon.js"></script>
 <!-- Sidebar jquery-->
 <!-- Plugins JS start-->
 <script src="http://laravel.pixelstrap.com/xolo/assets/js/login.js"></script>
 <!-- Plugins JS Ends-->
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/sidebar-menu.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/config.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/sidebar-menu.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/config.js"></script>
 <!-- Theme js-->
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/script.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/script.js"></script>
 <!-- login js-->
 <!-- Plugin used-->
 </body>
