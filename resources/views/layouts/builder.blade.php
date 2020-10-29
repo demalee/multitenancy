cd4f1a604a4a0f53d1c8d8df07efa69151d1415b
dev.demalee@gmail.com <dev.demalee@gmail.com>
2020-10-30 02:20:19 +0300

    https on pagebuilder

--- resources/views/layouts/builder.blade.php
+++ resources/views/layouts/builder.blade.php
@@ -2,38 +2,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta https-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta https-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Xolo admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Xolo admin template, dashboard template, flat admin template, responsive admin template, web app (Laravel 8)">
     <meta name="author" content="pixelstrap">
-    <link rel="icon" href="http://laravel.pixelstrap.com/xolo/assets/images/favicon.png" type="image/x-icon">
-    <link rel="shortcut icon" href="http://laravel.pixelstrap.com/xolo/assets/images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="https://laravel.pixelstrap.com/xolo/assets/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="https://laravel.pixelstrap.com/xolo/assets/images/favicon.png" type="image/x-icon">
     <title>Xolo - Premium Admin Template Page Builder</title>
     <!-- Google font-->
-    <link href="http://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i&amp;display=swap" rel="stylesheet">
-    <link href="http://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700&amp;display=swap" rel="stylesheet">
     <!-- Font Awesome-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/fontawesome.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/fontawesome.css">
     <!-- ico-font-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/icofont.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/icofont.css">
     <!-- Themify icon-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/themify.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/themify.css">
     <!-- Flag icon-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/flag-icon.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/flag-icon.css">
     <!-- Feather icon-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/feather-icon.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/feather-icon.css">
     <!-- Plugins css start-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/page-builder.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/page-builder.css">
     <!-- Plugins css Ends-->
     <!-- Bootstrap css-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/bootstrap.css">
     <!-- App css-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/style.css">
-    <link id="color" rel="stylesheet" href="http://laravel.pixelstrap.com/xolo/assets/css/color-1.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/style.css">
+    <link id="color" rel="stylesheet" href="https://laravel.pixelstrap.com/xolo/assets/css/color-1.css" media="screen">
     <!-- Responsive css-->
-    <link rel="stylesheet" type="text/css" href="http://laravel.pixelstrap.com/xolo/assets/css/responsive.css">         </head>
+    <link rel="stylesheet" type="text/css" href="https://laravel.pixelstrap.com/xolo/assets/css/responsive.css">         </head>
 <body>
 <!-- Loader starts-->
 <div class="loader-wrapper">
@@ -156,27 +156,27 @@
         </footer>         </div>
 </div>
 <!-- latest jquery-->
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/jquery-3.2.1.min.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/jquery-3.2.1.min.js"></script>
 <!-- Bootstrap js-->
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/bootstrap/popper.min.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/bootstrap/bootstrap.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/bootstrap/popper.min.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/bootstrap/bootstrap.js"></script>
 <!-- feather icon js-->
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/icons/feather-icon/feather.min.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/icons/feather-icon/feather-icon.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/icons/feather-icon/feather.min.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/icons/feather-icon/feather-icon.js"></script>
 <!-- Sidebar jquery-->
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/sidebar-menu.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/config.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/sidebar-menu.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/config.js"></script>
 <!-- Plugins JS start-->
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/editor/ckeditor/ckeditor.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/editor/ckeditor/adapters/jquery.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/jquery.ui.min.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/page-builder/jquery.grideditor.min.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/page-builder/page-builder-custom.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/tooltip-init.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/editor/ckeditor/ckeditor.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/editor/ckeditor/adapters/jquery.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/jquery.ui.min.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/page-builder/jquery.grideditor.min.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/page-builder/page-builder-custom.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/tooltip-init.js"></script>
 <!-- Plugins JS Ends-->
 <!-- Theme js-->
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/script.js"></script>
-<script src="http://laravel.pixelstrap.com/xolo/assets/js/theme-customizer/customizer.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/script.js"></script>
+<script src="https://laravel.pixelstrap.com/xolo/assets/js/theme-customizer/customizer.js"></script>
 <!-- login js-->
 <!-- Plugin used -->   </body>
 </html>
