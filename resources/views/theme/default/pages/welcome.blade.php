b949a97f31fecd60ab18eed65dd7a9b79c1e1f38
allanboyd <allanochieng307@gmail.com>
2020-11-11 13:47:50 +0300

    updated

--- resources/views/theme/default/pages/welcome.blade.php
+++ resources/views/theme/default/pages/welcome.blade.php
@@ -59,7 +59,19 @@
                                 <h2 contenteditable="true">20+ years <br>agricultural<br>cooperative</h2>
                                 <p class="mb-20">
                                     The automated process starts as soon as your clothes go into the machine. The outcome is
-                                    gleaming clothes!
+                                    gleaming clothes<!doctype html>
+                                    <html lang="en">
+                                    <head>
+                                        <meta charset="UTF-8">
+                                        <meta name="viewport"
+                                              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                                        <title>Document</title>
+                                    </head>
+                                    <body>
+                                    
+                                    </body>
+                                    </html>
                                 </p>
                                 <p class="mb-45">The automated process starts as soon as your clothes go into the machine.
                                     The outcome is gleaming clothes!</p>
