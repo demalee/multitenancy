324be9849350b5d7f3e05c5b382b07f96722b33b
allanboyd <allanochieng307@gmail.com>
2021-01-07 00:50:55 +0300

    update

--- public/certbort.sh
+++ public/certbort.sh
@@ -1,8 +1,3 @@
-#service nginx stop
-#<<<<<<< HEAD
-#find / -type f -name ".certbot.lock" exec rm {} \
-#certbot certonly --nginx  --preferred-challenges http  -d $1
-#=======
-#certbot certonly --standalone --preferred-challenges tls-sni -d $1
-#>>>>>>> ab88236ef8a50686609566208d177b8123ae4959
-#service nginx start
+service nginx stop
+certbot certonly --standalone --preferred-challenges http -d $1
+service nginx start
